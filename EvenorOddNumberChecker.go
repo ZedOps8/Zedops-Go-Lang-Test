ede5d855fdb2222df8109d43f1f838ed3700bb29
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2023-11-06 23:47:36 +0530

    Initial commit

---
+++ EvenorOddNumberChecker.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    num := 7
+    if num%2 == 0 {
+        fmt.Printf("%d is even\n", num)
+    } else {
+        fmt.Printf("%d is odd\n", num)
+    }
+}
