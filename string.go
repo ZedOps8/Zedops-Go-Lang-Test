bf8926db59dca7fb7dfe1eee80b030bf8e74afd8
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2023-12-07 09:35:27 +0530

    Initial commit

---
+++ string.go
@@ -0,0 +1,17 @@
+//This code compares two strings to check if they are equal and prints the result.
+
+
+package main
+
+import "fmt"
+
+func main() {
+    str1 := "apple"
+    str2 := "banana"
+
+    if str1 == str2 {
+        fmt.Println("The strings are equal.")
+    } else {
+        fmt.Println("The strings are not equal.")
+    }
+}
