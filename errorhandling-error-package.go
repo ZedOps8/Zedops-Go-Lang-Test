7727d982566288448bb860e46decbe531769f450
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2023-12-24 06:36:23 +0530

    Initial Commit

---
+++ errorhandling-error-package.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "errors"
+    "fmt"
+)
+
+func divide(a, b int) (int, error) {
+    if b == 0 {
+        return 0, errors.New("division by zero")
+    }
+    return a / b, nil
+}
+
+func main() {
+    result, err := divide(10, 2)
+    if err != nil {
+        fmt.Println("Error:", err)
+    } else {
+        fmt.Println("Result:", result)
+    }
+}
+//A code example demonstrating error handling using the errors package in Go:
