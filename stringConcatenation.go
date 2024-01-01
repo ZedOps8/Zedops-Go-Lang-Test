8f76574a4216eece4e218bc3459fde92cdc69e25
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2024-01-01 12:32:32 +0530

    Initial commit

---
+++ stringConcatenation.go
@@ -0,0 +1,12 @@
+//This code shows how to concatenate strings to create a full name from the first and last names
+
+package main
+
+import "fmt"
+
+func main() {
+    firstName := "John"
+    lastName := "Doe"
+    fullName := firstName + " " + lastName
+    fmt.Println("Full Name:", fullName)
+}
